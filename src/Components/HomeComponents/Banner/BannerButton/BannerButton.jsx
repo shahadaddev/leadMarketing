ecd832c541faf643be492314976b2ec881b7cb58
mdshahadad <identity.shahadad@gmail.com>
2025-09-09 02:35:30 +0600

    {Banner} Banner button responsive

--- src/Components/HomeComponents/Banner/BannerButton/BannerButton.jsx
+++ src/Components/HomeComponents/Banner/BannerButton/BannerButton.jsx
@@ -4,10 +4,10 @@ import { NavLink } from "react-router";
 
 const BannerButton = () => {
   return (
-    <div className="flex justify-center gap-8 py-16">
+    <div className="md:flex md:flex-row sm:flex-col justify-center gap-8 md:py-16 pt-12 md:space-y-0 space-y-4">
       <div>
         <NavLink to={"/contact"}>
-          <h3 className="text-lg font-bold bg-[#F40026] text-white px-10 py-4 rounded-xl flex items-center justify-center cursor-pointer hover:bg-white/30 hover:backdrop-blur-md border-2 border-[#F40026] hover:text-[#F40026] transition-all duration-1000 hover-container w-72">
+          <h3 className="md:text-lg font-bold bg-[#F40026] text-white px-10 py-4 rounded-xl flex items-center justify-center cursor-pointer hover:bg-white/30 hover:backdrop-blur-md border-2 border-[#F40026] hover:text-[#F40026] transition-all duration-1000 hover-container md:w-72 w-3/4 mx-auto">
             Start A Journey
             <GoArrowUpRight className="text-3xl font-bold hover-arrow" />
           </h3>
@@ -15,7 +15,7 @@ const BannerButton = () => {
       </div>
       <div>
         <NavLink to={"https://leadbangladesh.com/"} target="_blank">
-          <h3 className="text-lg font-bold bg-white/30 backdrop-blur-md text-[#F40026] border-2 border-[#F40026] hover:bg-[#F40026] hover:text-white px-8 py-4 rounded-xl transition-all duration-1000 hover-container flex items-center justify-center cursor-pointer w-72">
+          <h3 className="md:text-lg font-bold bg-white/30 backdrop-blur-md text-[#F40026] border-2 border-[#F40026] hover:bg-[#F40026] hover:text-white px-8 py-4 rounded-xl transition-all duration-1000 hover-container flex items-center justify-center cursor-pointer md:w-72 w-3/4 mx-auto">
             Visit Lead Bangladesh{" "}
             <GoArrowUpRight className="text-3xl font-extrabold hover-arrow" />
           </h3>
