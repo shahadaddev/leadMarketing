134076902452ca67a0c8776e925f204979466a12
mdshahadad <identity.shahadad@gmail.com>
2025-09-13 01:03:19 +0600

    {works} Works slider added

--- src/Components/OurWorksComponents/WorksButton/WorksButtonHome.jsx
+++ src/Components/OurWorksComponents/WorksButton/WorksButtonHome.jsx
@@ -7,7 +7,7 @@ const WorksButtonHome = () => {
     <div className="lg:w-full w-5/6 mx-auto">
       <NavLink to={"/works"}>
         <h3 className="lg:text-lg font-bold bg-white text-[#F40026] px-10 py-4 rounded-xl flex items-center justify-center cursor-pointer hover:bg-[#F40026] hover:backdrop-blur-md border-2 border-[#F40026] hover:text-white transition-all duration-700 hover-container gap-2">
-          Explore Our Works{" "}
+          Our All Works{" "}
           <GoArrowUpRight className="text-3xl font-bold hover-arrow" />
         </h3>
       </NavLink>
--- src/Components/OurWorksComponents/WorksTitle/WorksTitle.jsx
+++ src/Components/OurWorksComponents/WorksTitle/WorksTitle.jsx
@@ -1,9 +1,15 @@
+import { HiBadgeCheck } from "react-icons/hi";
 import WorksButtonHome from "../WorksButton/WorksButtonHome";
 
 const WorksTitle = () => {
   return (
-    <div className="flex lg:flex-col justify-start lg:justify-between items-end h-full w-full">
-      <div className="text-left justify-start flex">
+    <div className="flex lg:flex-col justify-start lg:justify-between h-full w-full">
+      <div className="space-y-4">
+        <h5 className="flex items-center justify-center gap-1 bg-red-200 lg:w-[140px] w-[140px] lg:py-1.5 py-1 rounded-full text-red-600 border-1 border-red-600 font-semibold lg:text-md text-sm">
+          {" "}
+          <HiBadgeCheck className="lg:text-xl text-lg" />
+          Our Works
+        </h5>
         <h2 className="xl:text-[80px] lg:text-7xl text-5xl font-black uppercase xl:leading-17 lg:leading-14 leading-9 text-gray-700">
           The <br /> <span className="text-[#F40026]">stories</span> <br />{" "}
           We've <br /> built
